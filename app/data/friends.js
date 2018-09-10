cec22f41e561dab48d9ffaa37f1ec3dfbd54783f
Abdul Said <abdulrahman.asaid@gmail.com>
2018-09-10 12:30:50 -0400

    added html pages

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friendsArray = [
+  {
+    name: 'John Doe',
+    photo:
+      'https://www.google.ca/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjp_f57LDdAhWs34MKHakLC-8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.vexels.com%2Feditables%2Fpreview%2F145908%2Fmale-avatar-maker&psig=AOvVaw001oHNYxS19hW7QqJh91B6&ust=1536683117683615',
+    scores: ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5']
+  }
+];
