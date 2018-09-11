2cacd8a427bd96d6d3bee80eeb3526590d71e2bd
Abdul Said <abdulrahman.asaid@gmail.com>
2018-09-11 18:38:14 -0400

    fixed images

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,37 +4,37 @@ var friendsArray = [
   {
     name: 'John Doe',
     photo:
-      'https://www.google.ca/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjp_f57LDdAhWs34MKHakLC-8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.vexels.com%2Feditables%2Fpreview%2F145908%2Fmale-avatar-maker&psig=AOvVaw001oHNYxS19hW7QqJh91B6&ust=1536683117683615',
+      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
     scores: ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5']
   },
   {
     name: 'Jane Doe',
     photo:
-      'https://www.google.ca/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjp_f57LDdAhWs34MKHakLC-8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.vexels.com%2Feditables%2Fpreview%2F145908%2Fmale-avatar-maker&psig=AOvVaw001oHNYxS19hW7QqJh91B6&ust=1536683117683615',
+      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
     scores: ['4', '4', '4', '4', '4', '4', '4', '4', '4', '4']
   },
   {
     name: 'Guy Anderson',
     photo:
-      'https://www.google.ca/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjp_f57LDdAhWs34MKHakLC-8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.vexels.com%2Feditables%2Fpreview%2F145908%2Fmale-avatar-maker&psig=AOvVaw001oHNYxS19hW7QqJh91B6&ust=1536683117683615',
+      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
     scores: ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
   },
   {
     name: 'Hailey Jones',
     photo:
-      'https://www.google.ca/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjp_f57LDdAhWs34MKHakLC-8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.vexels.com%2Feditables%2Fpreview%2F145908%2Fmale-avatar-maker&psig=AOvVaw001oHNYxS19hW7QqJh91B6&ust=1536683117683615',
+      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
     scores: ['2', '2', '2', '2', '2', '2', '2', '2', '2', '2']
   },
   {
     name: 'Whitney Brown',
     photo:
-      'https://www.google.ca/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjp_f57LDdAhWs34MKHakLC-8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.vexels.com%2Feditables%2Fpreview%2F145908%2Fmale-avatar-maker&psig=AOvVaw001oHNYxS19hW7QqJh91B6&ust=1536683117683615',
+      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
     scores: ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
   },
   {
     name: 'Brown White',
     photo:
-      'https://www.google.ca/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjp_f57LDdAhWs34MKHakLC-8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.vexels.com%2Feditables%2Fpreview%2F145908%2Fmale-avatar-maker&psig=AOvVaw001oHNYxS19hW7QqJh91B6&ust=1536683117683615',
+      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
     scores: ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5']
   }
 ];
