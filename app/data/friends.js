613adacd5c054c3bbf17e54c4a236cde329892ae
Abdul Said <abdulrahman.asaid@gmail.com>
2018-09-12 09:42:21 -0400

    fixed database

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,38 +4,37 @@ var friendsArray = [
   {
     name: 'John Doe',
     photo:
-      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
-    scores: ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5']
+      'https://media.tenor.com/images/30bd60f17534debe0ecf1455ee6d309d/tenor.png',
+    scores: ['5', '4', '3', '3', '2', '1', '4', '5', '3', '1']
   },
   {
     name: 'Jane Doe',
     photo:
-      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
-    scores: ['4', '4', '4', '4', '4', '4', '4', '4', '4', '4']
+      'https://media.istockphoto.com/photos/portrait-of-fashion-hipster-cool-girl-in-sunglasses-and-colorful-picture-id491752052?k=6&m=491752052&s=612x612&w=0&h=XcYFJ1am5qyaOt4xB4J5ywqxMG1GZMlegqNrNHFZ8hQ=',
+    scores: ['4', '4', '1', '2', '3', '3', '5', '2', '4', '3']
   },
   {
     name: 'Guy Anderson',
     photo:
       'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
-    scores: ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
+    scores: ['3', '4', '5', '5', '2', '1', '3', '4', '3', '1']
   },
   {
     name: 'Hailey Jones',
     photo:
-      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
-    scores: ['2', '2', '2', '2', '2', '2', '2', '2', '2', '2']
+      'https://d3lp4xedbqa8a5.cloudfront.net/s3/digital-cougar-assets/HarpersBazaar/2016/12/20/170174/0-cool-girl-haircut.jpg?width=600&height=315&quality=75&mode=crop',
+    scores: ['3', '4', '1', '1', '2', '1', '3', '5', '5', '3']
   },
   {
     name: 'Whitney Brown',
     photo:
-      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
-    scores: ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
+      'https://imgix.bustle.com/uploads/image/2018/9/11/864d5186-8612-40f6-8166-2eef6744d573-ganni.jpg?w=990&h=600&fit=crop&crop=faces&auto=format&q=70',
+    scores: ['1', '2', '3', '5', '1', '2', '3', '1', '3', '2']
   },
   {
     name: 'Brown White',
-    photo:
-      'http://www.travellingwiththekids.com/wp-content/uploads/2013/06/COOL-DUD-SUNGLASSES-853x1137.jpg',
-    scores: ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5']
+    photo: 'https://thumbs.dreamstime.com/b/cool-guy-sunglasses-17974680.jpg',
+    scores: ['4', '5', '2', '3', '5', '1', '2', '5', '3', '5']
   }
 ];
 
